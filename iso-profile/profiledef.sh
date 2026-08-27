2827d587f90ebf014676628dfc363a82586a38b2
Alfredo2364 <joaquintek221@gmail.com>
2026-08-27 01:17:16 -0600

    Corregir sintaxis oficial de buildmodes y agregar syslinux a profiledef y dependencias

--- iso-profile/profiledef.sh
+++ iso-profile/profiledef.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
 # Profile definition for Dzulux Linux
 # Designed and Crafted by Alfredo Dzul
 
@@ -8,23 +9,17 @@ iso_publisher="Alfredo Dzul <https://github.com/Alfredo2364/Linux>"
 iso_application="Dzulux Linux Live/Installer OS (Beta)"
 iso_version="1.0-beta"
 install_dir="dzulux"
-build_modes=('iso')
-bootmodes=(
-    'bios.syslinux.mbr'
-    'bios.syslinux.eltorito'
-    'uefi-x64.grub.esp'
-    'uefi-x64.grub.eltorito'
-)
-arch="x86_64"
+buildmodes=('iso')
+bootmodes=('bios.syslinux'
+           'uefi.grub')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd')
 
-# Custom permissions for system scripts and configurations in airootfs
 file_permissions=(
-    ["/usr/local/bin/dzulux-installer"]="0:0:755"
-    ["/usr/local/bin/hw-detect"]="0:0:755"
-    ["/usr/local/bin/switch-mode"]="0:0:755"
-    ["/usr/local/bin/dzulux-welcome"]="0:0:755"
-    ["/usr/local/bin/dzulux-live-init"]="0:0:755"
+  ["/usr/local/bin/dzulux-installer"]="0:0:755"
+  ["/usr/local/bin/hw-detect"]="0:0:755"
+  ["/usr/local/bin/switch-mode"]="0:0:755"
+  ["/usr/local/bin/dzulux-welcome"]="0:0:755"
+  ["/usr/local/bin/dzulux-live-init"]="0:0:755"
 )

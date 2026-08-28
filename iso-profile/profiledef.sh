c32d3c5f89fca9098dc115bd3b093cfe9d864354
Alfredo2364 <joaquintek221@gmail.com>
2026-08-28 08:31:10 -0600

    Corregir fallo critico de arranque 'Failed to start Switch Root': agregar mkinitcpio-archiso, hooks archiso, linux.preset, archisosearchuuid y desbloqueo de root shadow

--- iso-profile/profiledef.sh
+++ iso-profile/profiledef.sh
@@ -17,6 +17,8 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 
 file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
   ["/usr/local/bin/dzulux-installer"]="0:0:755"
   ["/usr/local/bin/hw-detect"]="0:0:755"
   ["/usr/local/bin/switch-mode"]="0:0:755"
